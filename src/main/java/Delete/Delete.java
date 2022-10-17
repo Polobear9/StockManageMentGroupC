63222ea1eb587ef56b309db3495d209b18948e43
Polobear9 <lieams1996@gmail.com>
2022-10-17 23:59:47 +0900

    Add the detail method All data , ID, Name , number.

--- src/main/java/Delete/Delete.java
+++ src/main/java/Delete/Delete.java
@@ -4,23 +4,27 @@ import java.util.Scanner;
 
 public class Delete {
     public static void delete() {
-    Scanner sc = new Scanner(System.in);
-    int menuNumber = 0;
+        Scanner sc = new Scanner(System.in);
+        int menuNumber;
 
-        while(true){
+        while (true) {
             menuNumber = Delete.Menu(sc);
-            switch (menuNumber){
+            switch (menuNumber) {
                 case 1:
                     System.out.println("1. Delete All DataBase");
+                    delete_AllData();
                     break;
                 case 2:
                     System.out.println("2. Delete By ID");
+                    delete_ById();
                     break;
                 case 3:
                     System.out.println("3. Delete By NAME");
+                    delete_ByName();
                     break;
                 case 4:
                     System.out.println("4. Delete By NUMBER");
+                    delete_ByNumber();
                     break;
             }
             break;
@@ -37,6 +41,22 @@ public class Delete {
         System.out.println("4. Delete NUMBER");
         System.out.println(" -------------------------------- ");
         return sc.nextInt();
+    }
+
+    private static void delete_AllData() {
+        System.out.println("delete_AllData");
+    }
+
+    private static void delete_ById() {
+        System.out.println("delete_ById");
+    }
+
+    private static void delete_ByName() {
+        System.out.println("delete_ByName");
+    }
+
+    private static void delete_ByNumber() {
+        System.out.println("delete_ByNumber");
     }
 
 }
