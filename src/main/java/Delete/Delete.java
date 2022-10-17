0b4c2ccd45111caa1044f364620219c57aef0335
Polobear9 <lieams1996@gmail.com>
2022-10-17 11:12:29 +0900

    Add the menu

--- src/main/java/Delete/Delete.java
+++ src/main/java/Delete/Delete.java
@@ -1,4 +1,13 @@
 package Delete;
 
 public class Delete {
+    public static void main(String[] args) {
+        System.out.println(" -------------------------------- ");
+        System.out.println("Delete Menu");
+        System.out.println("1. Delete All");
+        System.out.println("2. Delete ID");
+        System.out.println("1. Delete JAN");
+        System.out.println("1. Delete NAME");
+    }
+
 }
