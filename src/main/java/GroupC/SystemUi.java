bd213ed415c1445a6a247ef763e1234392dbadc5
Polobear9 <lieams1996@gmail.com>
2022-10-19 10:12:55 +0900

    Create the Switch sentence in SystemUi class.

--- src/main/java/GroupC/SystemUi.java
+++ src/main/java/GroupC/SystemUi.java
@@ -7,7 +7,20 @@ public class SystemUi {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please Select the Menu");
         System.out.println("[メニュー] 1:検索 2:登録 3:変更 4:削除 0:終了>");
-        scanner.nextLine();
+        String menu_Number = scanner.nextLine();
+
+        switch (menu_Number){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "0":
+                break;
+        }
 
     }
 }
